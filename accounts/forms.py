8b807360c5de7ab765f840e88edaa0435ff87ca7
kevin <kevin.madrigali@stud.unifi.it>
2023-06-13 11:13:11 +0200

    Minor changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,7 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         self.fields['email'].required = True
+        self.fields['birth'].required = True
 
     class Meta:
         model = CustomUser
